3edff2530a075aa329c59d309c084b757759c460
Lyndsey Browning <lbrowning86@gmail.com>
2016-06-13 19:27:31 +0100

    Set up React Router with PhotoGrid and Single component Routes.

--- src/reduxstagram.js
+++ src/reduxstagram.js
@@ -6,5 +6,19 @@ import './sass/main.scss';
 
 // compoments
 import Main from './components/Main';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
 
-render(<Main />, document.getElementById('root'));
+// react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path='/' component={Main}>
+     <IndexRoute component={PhotoGrid} />
+     <Route path='/view/:postId' component={Single} />
+    </Route>
+  </Router>
+);
+
+render(router, document.getElementById('root'));
