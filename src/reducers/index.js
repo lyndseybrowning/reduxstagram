8536cea81490a0e11c535eaa3d7e4c1282849394
Lyndsey Browning <lbrowning86@gmail.com>
2016-06-13 20:01:13 +0100

    Added reducers with rootReducer to combine all

---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+// one reducer handles all reducers
+// because you can't have multiple reducers
+// called from separate files
+
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+import posts from './posts';
+import comments from './comments';
+
+const rootReducer = combineReducers({
+  posts,
+  comments,
+  router: routerReducer
+});
+
+export default rootReducer;
---
+++ src/reducers/posts.js
@@ -0,0 +1,13 @@
+// a reducer takes in two things
+// 1. the action
+// 2. a copy of the current state
+// reducer returns an updated copy of the store
+// the first time it runs, state will be empty which is
+// why we set up the default parameter
+
+function posts(state = [], action) {
+  console.log(state, action);
+  return state;
+}
+
+export default posts;
