04efd217a7e19d01d0ef791aab7464bc469b47a4
Lyndsey Browning <lbrowning86@gmail.com>
2016-06-13 18:48:12 +0100

    Added main component

--- src/reduxstagram.js
+++ src/reduxstagram.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
 
+// import styles
+import './sass/main.scss';
+
+// compoments
+import Main from './components/Main';
+
+render(<Main />, document.getElementById('root'));
