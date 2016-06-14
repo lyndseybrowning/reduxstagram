4e98cec6262b9cddb26cd7f41cc08cbf2a23ff14
Lyndsey Browning <lbrowning86@gmail.com>
2016-06-14 18:53:18 +0100

    Display Comments component and pass all comments down through the Single component

--- src/components/Single.js
+++ src/components/Single.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import Photo from './Photo';
-// import Comments from './Comments';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
     // index of the post
     // accessed from URL bar
-    const index = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const { postId } = this.props.params;
+    const index = this.props.posts.findIndex((post) => post.code === postId);
 
     // get us the post
     const post = this.props.posts[index];
+    // find comments
+    const postComments = this.props.comments[postId] || [];
 
     return (
       <div className='single-photo'>
         <Photo index={index} post={post} {...this.props} />
+        <Comments postComments={postComments} />
       </div>
     );
   }
