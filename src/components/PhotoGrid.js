e4666716410d27a38274d6b9d22d4eea8fe971b9
Lyndsey Browning <lbrowning86@gmail.com>
2016-06-14 18:00:45 +0100

    Added Photo component

--- src/components/PhotoGrid.js
+++ src/components/PhotoGrid.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
   render() {
+    console.log(this.props.posts);
     return (
       <div className='photo-grid'>
-        Photo Grid        
+        {this.props.posts.map((post, index) => <Photo />)}
       </div>
     );
   }
