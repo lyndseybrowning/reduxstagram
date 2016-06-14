3db2ad7ada914beed775c5a97b5cff01e5308010
Lyndsey Browning <lbrowning86@gmail.com>
2016-06-14 19:06:52 +0100

    Created ADD_COMMENT action and reducer

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -13,11 +13,21 @@ const Comments = React.createClass({
     );
   },
 
+  handleSubmit(event) {
+    event.preventDefault();
+
+    let { postId } = this.props.params;
+    let author = this.refs.author.value;
+    let comment = this.refs.comment.value;
+
+    this.props.addComment(postId, author, comment);
+  },
+
   render() {
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComments)}
-        <form ref='commentForm' className='comment-form'>
+        <form ref='commentForm' className='comment-form' onSubmit={this.handleSubmit}>
           <input type='text' ref='author' placeholder='author' />
           <input type='text' ref='comment' placeholder='comment' />
           <input type='submit' hidden />
--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,5 +1,31 @@
+function postComments(state = [], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT':
+      // return existing state with the new comment
+      return [
+        ...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      return state;
+    default:
+      return state;
+  }
+}
+
 function comments(state = [], action) {
-  console.log(state, action);
+  if(typeof action.postId !== 'undefined') {
+    return {
+      // take the current state
+      ...state,
+      // overwrite post with a new one
+      // we send in all of the existing comments
+      // and postComments handles it by using existing state
+      // with a newly generated object for the new comment
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state;
 }
 
