65fe131d18a5f824f565e876f8489eeaff6e0004
Lyndsey Browning <lbrowning86@gmail.com>
2016-06-14 18:40:40 +0100

    Rendered Photo component into Single component

--- src/components/Single.js
+++ src/components/Single.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Photo from './Photo';
+// import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    // index of the post
+    // accessed from URL bar
+    const index = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+
+    // get us the post
+    const post = this.props.posts[index];
+
     return (
       <div className='single-photo'>
-        Single
+        <Photo index={index} post={post} {...this.props} />
       </div>
     );
   }
