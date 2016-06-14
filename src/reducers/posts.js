bc9bf6804cee8fe9e090d58b7784a62535faa69a
Lyndsey Browning <lbrowning86@gmail.com>
2016-06-14 18:34:22 +0100

    Completed INCREMENT_POST reducer

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -6,9 +6,20 @@
 // why we set up the default parameter
 
 function posts(state = [], action) {
-  console.log("the post will change");
-  console.log(state, action);
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES':
+      // return updated state
+      // take the whole posts array
+      // modify the likes at index passed in via the action
+      // then return the new array
+      return [
+        ...state.slice(0, action.index), // take everything before the index we're updating
+        {...state[action.index], likes: state[action.index].likes + 1}, // take a copy of the existing index but update the likes
+        ...state.slice(action.index + 1), // take everything after the one we're updating
+      ]
+    default:
+      return state;
+  }
 }
 
 export default posts;
