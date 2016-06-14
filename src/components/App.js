e93bd4efd76f3b9b5947da9ce0472444aee5a365
Lyndsey Browning <lbrowning86@gmail.com>
2016-06-14 17:45:07 +0100

    Added some comments about redux connect()

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,8 @@ function mapDispatchedToProps(dispatch) {
   return bindActionCreators(actionCreators, dispatch);
 }
 
+// injects our data from our store into the main component
+// without having to pass props down every component
 const App = connect(mapStateToProps, mapDispatchedToProps)(Main);
 
 export default App;
